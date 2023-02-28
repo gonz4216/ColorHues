230737c024dfd6f64276e9265e481a5c9b22b568
gonz4216 <ethgon19@gmail.com>
2023-02-27 23:45:43 -0500

    first commiut

---
+++ ColorHues/View/Button.swift
@@ -0,0 +1,20 @@
+//
+//  Button.swift
+//  ColorHues
+//
+//  Created by Ethan Gonsalves on 2023-02-27.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ColorHues/View/Label.swift
@@ -0,0 +1,20 @@
+//
+//  Label.swift
+//  ColorHues
+//
+//  Created by Ethan Gonsalves on 2023-02-27.
+//
+
+import UIKit
+
+class Label: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
